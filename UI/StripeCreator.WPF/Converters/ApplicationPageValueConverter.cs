7368ae7d4195fe48e1f74976ee5b8516abef3263
Nike073i <nike073i@mail.ru>
2023-01-25 19:34:50 +0400

    Создал конвертер для ApplicationPage и привязал к окну данное значение

---
+++ UI/StripeCreator.WPF/Converters/ApplicationPageValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace StripeCreator.WPF
+{
+    class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (ApplicationPage)value switch
+            {
+                ApplicationPage.Welcome => new WelcomePage(IoC.GetRequiredService<WelcomePageViewModel>()),
+                _ => throw new ArgumentException("Получена несуществующая страница для конвертации"),
+            };
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
