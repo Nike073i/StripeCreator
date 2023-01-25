41f60f823a974f2884e8d18e75da5b22f035ae62
Nike073i <nike073i@mail.ru>
2023-01-25 19:28:39 +0400

    Добавление перечисления страниц приложения и ApplicationViewModel

---
+++ UI/StripeCreator.WPF/DataModels/ApplicationPage.cs
@@ -0,0 +1,13 @@
+﻿namespace StripeCreator.WPF
+{
+    /// <summary>
+    /// Перечисление страниц приложения
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// Стартовая страница приветствия
+        /// </summary>
+        Welcome = 0
+    }
+}
---
+++ UI/StripeCreator.WPF/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace StripeCreator.WPF
+{
+    public class ApplicationViewModel : BaseViewModel
+    {
+        public ApplicationPage CurrentPage { get; private set; }
+
+        /// <summary>
+        /// Смена страницы на <paramref name="page"/>
+        /// </summary>
+        /// <param name="page">Новая страница</param>
+        public void GoToPage(ApplicationPage page)
+        {
+            // Возможна дополнительная логика проверки перехода
+            CurrentPage = page;
+        }
+    }
+}
