9b1797e4f7c52e34fb573509c8360f5e012ebec8
Nike073i <nike073i@mail.ru>
2023-01-26 14:30:57 +0400

    Добавление пунктов меню

--- UI/StripeCreator.WPF/ViewModels/Pages/DataStorePageViewModel.cs
+++ UI/StripeCreator.WPF/ViewModels/Pages/DataStorePageViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace StripeCreator.WPF
+﻿using FontAwesome5;
+using System.Collections.Generic;
+
+namespace StripeCreator.WPF
 {
     /// <summary>
     /// Класс view-model для страницы работы со справочниками
@@ -7,19 +10,42 @@
     {
         #region Constants
 
+        private readonly string _header = "Справочники";
+
         #endregion
 
         #region Public Properties 
+
+        public ActionMenuViewModel ActionMenuViewModel { get; protected set; }
 
         #endregion
 
         #region Constructors
 
-        public DataStorePageViewModel() { }
+        public DataStorePageViewModel()
+        {
+            ActionMenuViewModel = new()
+            {
+                Header = _header,
+                Items = GetSideMenuItems()
+            };
+        }
 
         #endregion
 
         #region Private helper methods
+
+        private List<ActionMenuItemViewModel> GetSideMenuItems()
+        {
+            return new List<ActionMenuItemViewModel>
+            {
+                new(EFontAwesomeIcon.Solid_Bars, "Нитки", ShowThreadStore),
+                new(EFontAwesomeIcon.Solid_CropAlt, "Ткани",ShowClothStore),
+            };
+        }
+
+        private void ShowThreadStore(object? parameter) { }
+        private void ShowClothStore(object? parameter) { }
 
         #endregion
     }
