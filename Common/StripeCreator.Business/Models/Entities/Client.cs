846b1f9ed332801d7e33c7e39580fa63979d505f
Nike073i <nike073i@mail.ru>
2023-02-01 19:43:48 +0400

    Класс сущности клиента

---
+++ Common/StripeCreator.Business/Models/Entities/Client.cs
@@ -0,0 +1,39 @@
+using StripeCreator.Core.Models;
+
+namespace StripeCreator.Business.Models
+{
+    /// <summary>
+    /// Класс сущности клиента
+    /// </summary>
+    public class Client : Entity
+    {
+        #region Public properties
+
+        /// <summary>
+        /// Персональные данные клиента
+        /// </summary>
+        public PersonData PersonData { get; set; }
+
+        /// <summary>
+        /// Контактные данные клиента
+        /// </summary>
+        public ContactData ContactData { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор с полной инициализацией
+        /// </summary>
+        /// <param name="personData">Персональные данные</param>
+        /// <param name="contactData">Контактные данные</param>
+        public Client(PersonData personData, ContactData contactData, Guid? id = null) : base(id)
+        {
+            PersonData = personData;
+            ContactData = contactData;
+        }
+
+        #endregion
+    }
+}
