6d73bf1fec630154e801e4b0702d5ee82ad978f0
Nike073i <nike073i@mail.ru>
2023-02-01 18:13:08 +0400

    Базовые элементы Entity и IRepositories

---
+++ Common/StripeCreator.Core/Models/Entity.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace StripeCreator.Core.Models
+{
+    /// <summary>
+    /// Базовый класс сущности
+    /// </summary>
+    public abstract class Entity
+    {
+        #region Public properties 
+
+        /// <summary>
+        /// Уникальный идентификатор сущности. Если сущность новая, то <paramref name="Id"/> равен null
+        /// </summary>
+        public Guid? Id { get; protected set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        /// <param name="id">Идентификатор сущности</param>
+        public Entity(Guid? id = null) => Id = id;
+
+        #endregion
+    }
+}
