d213740e0ffaaacaa21515fcce12e8c2deab4748
Nike073i <nike073i@mail.ru>
2023-02-02 23:06:03 +0400

    Создание моделей нитей и тканей

---
+++ Common/StripeCreator.Stripe/Models/Materials/ThreadType.cs
@@ -0,0 +1,38 @@
+namespace StripeCreator.Stripe.Models
+{
+    /// <summary>
+    /// Тип нити
+    /// </summary>
+    public enum ThreadType
+    {
+        /// <summary>
+        /// Нитки Мулине
+        /// </summary>
+        Muline,
+
+        /// <summary>
+        /// Гобелен
+        /// </summary>
+        Tapestry,
+
+        /// <summary>
+        /// Хлопчатобумажные
+        /// </summary>
+        Cotton,
+
+        /// <summary>
+        /// Шелковые
+        /// </summary>
+        Silk,
+
+        /// <summary>
+        /// Универсальные
+        /// </summary>
+        Universal,
+
+        /// <summary>
+        /// Металлик
+        /// </summary>
+        Metallic
+    }
+}
