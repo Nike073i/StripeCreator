868afdd705a7cd8d67aa677499661a374c0e43ce
Nike073i <nike073i@mail.ru>
2023-02-02 23:06:03 +0400

    Создание моделей нитей и тканей

---
+++ Common/StripeCreator.Stripe/Models/Materials/ClothType.cs
@@ -0,0 +1,38 @@
+namespace StripeCreator.Stripe.Models
+{
+    /// <summary>
+    /// Тип ткани
+    /// </summary>
+    public enum ClothType
+    {
+        /// <summary>
+        /// Аида
+        /// </summary>
+        Aida,
+
+        /// <summary>
+        /// Канва равномерного плетения, "Равномерка"
+        /// </summary>
+        Uniform,
+
+        /// <summary>
+        /// Страмин
+        /// </summary>
+        Stramin,
+
+        /// <summary>
+        /// Пластиковая канва
+        /// </summary>
+        Plastic,
+
+        /// <summary>
+        /// Накладная канва
+        /// </summary>
+        False,
+
+        /// <summary>
+        /// Растворимая канва
+        /// </summary>
+        Soluble
+    }
+}
