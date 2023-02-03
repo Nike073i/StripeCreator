0587373e8b59b95b12b1c6877edf4223cc4a328a
Nike073i <nike073i@mail.ru>
2023-02-03 19:45:20 +0400

    Интерфейс хранителя данных

---
+++ Common/StripeCreator.Stripe/Services/IDataKeeper.cs
@@ -0,0 +1,24 @@
+namespace StripeCreator.Stripe.Services
+{
+    /// <summary>
+    /// Интерфейс хранителя данных
+    /// </summary>
+    /// <typeparam name="TData">Тип хранимых данных</typeparam>
+    public interface IDataKeeper<TData>
+    {
+        /// <summary>
+        /// Загрузка данных по абсолютному пути
+        /// </summary>
+        /// <param name="path">Абсолютный путь к данным</param>
+        /// <returns>Данные</returns>
+        Task<TData> LoadAsync(string path);
+
+        /// <summary>
+        /// Сохранение данных по абсолютному пути
+        /// </summary>
+        /// <param name="path">Абсолютный путь сохранения данных</param>
+        /// <param name="data">Сохраняемые данные</param>
+        /// <returns></returns>
+        Task SaveAsync(string path, TData data);
+    }
+}
