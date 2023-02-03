19c94dab6a89904c7bde1bf185d5699dab6e7dd0
Nike073i <nike073i@mail.ru>
2023-02-03 13:05:56 +0400

    Создание репозиториев материалов

---
+++ Common/StripeCreator.Stripe/Repositories/IClothRepository.cs
@@ -0,0 +1,7 @@
+using StripeCreator.Core.Repositories;
+using StripeCreator.Stripe.Models;
+
+namespace StripeCreator.Stripe.Repositories
+{
+    public interface IClothRepository : IRepository<Cloth> { }
+}
