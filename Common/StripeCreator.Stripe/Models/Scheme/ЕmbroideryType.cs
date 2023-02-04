4a138f02b9004f9fbdeae80b787567efe7a33d03
Nike073i <nike073i@mail.ru>
2023-02-04 19:16:18 +0400

    Создание перечисления видов вышивки

---
+++ Common/StripeCreator.Stripe/Models/Scheme/ЕmbroideryType.cs
@@ -0,0 +1,28 @@
+namespace StripeCreator.Stripe.Models
+{
+    /// <summary>
+    /// Виды вышивки
+    /// </summary>
+    public enum ЕmbroideryType
+    {
+        /// <summary>
+        /// Крестиком
+        /// </summary>
+        Cross,
+
+        /// <summary>
+        /// Гладью вбок
+        /// </summary>
+        SmoothHorizontal,
+
+        /// <summary>
+        /// Гладью вперед
+        /// </summary>
+        SmoothVertical,
+
+        /// <summary>
+        /// Гладью по диагонали
+        /// </summary>
+        SmoothDiagonal
+    }
+}
