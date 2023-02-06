4affbbc65df1aef086abb0987525d0fe65879631
Nike073i <nike073i@mail.ru>
2023-02-06 22:35:16 +0400

    Создание репозитория для тканей

---
+++ Data/StripeCreator.DAL/Mappers/ClothMapper.cs
@@ -0,0 +1,49 @@
+﻿using StripeCreator.Core.Models;
+using StripeCreator.DAL.Models;
+using StripeCreator.Stripe.Models;
+
+namespace StripeCreator.DAL.Mappers
+{
+    /// <summary>
+    /// преобразователя хранимых <see cref="DbCloth"/> и доменных <see cref="Cloth"/> сущностей 
+    /// </summary>
+    public class ClothMapper : IDbMapper<DbCloth, Cloth>
+    {
+        #region Interface implementations
+
+        public DbCloth CreateDbModel(Cloth domainModel)
+        {
+            string name = domainModel.Name;
+            decimal price = domainModel.Price;
+            string manufacturer = domainModel.Manufacturer;
+            var color = domainModel.Color.HexValue;
+            var type = domainModel.Type;
+            int count = domainModel.Count;
+            return new DbCloth(name, price, manufacturer, color, type, count);
+        }
+
+        public void UpdateDbModel(Cloth domainModel, ref DbCloth dbModel)
+        {
+            dbModel.Name = domainModel.Name;
+            dbModel.Price = domainModel.Price;
+            dbModel.Manufacturer = domainModel.Manufacturer;
+            dbModel.ColorHex = domainModel.Color.HexValue;
+            dbModel.Type = domainModel.Type;
+            dbModel.Count = domainModel.Count;
+        }
+
+        public Cloth MapToDomainModel(DbCloth dbModel)
+        {
+            string name = dbModel.Name;
+            decimal price = dbModel.Price;
+            string manufacturer = dbModel.Manufacturer;
+            var color = new Color(dbModel.ColorHex);
+            var type = dbModel.Type;
+            int count = dbModel.Count;
+            var id = dbModel.Id;
+            return new Cloth(name, price, manufacturer, color, type, count, id);
+        }
+
+        #endregion
+    }
+}
---
+++ Data/StripeCreator.DAL/Repositories/DbClothRepository.cs
@@ -0,0 +1,23 @@
+﻿using StripeCreator.DAL.Mappers;
+using StripeCreator.DAL.Models;
+using StripeCreator.Stripe.Models;
+using StripeCreator.Stripe.Repositories;
+
+namespace StripeCreator.DAL.Repositories
+{
+    /// <summary>
+    /// Реализация интерфейса сущности <see cref="Cloth"/>
+    /// </summary>
+    public class DbClothRepository : DbRepository<DbCloth, Cloth>, IClothRepository
+    {
+        #region Constructors 
+
+        /// <summary>
+        /// Конструктор с полной инициаилизацией
+        /// </summary>
+        /// <param name="context">Контекст БД</param>
+        public DbClothRepository(StripeCreatorDb context) : base(context, new ClothMapper()) { }
+
+        #endregion
+    }
+}
