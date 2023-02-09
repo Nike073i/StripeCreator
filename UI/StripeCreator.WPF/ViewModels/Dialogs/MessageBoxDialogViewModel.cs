96697277575ae925e6ec619ef1078f60a43d70ee
Nike073i <nike073i@mail.ru>
2023-02-09 19:08:47 +0400

    VM для MessageBox

---
+++ UI/StripeCreator.WPF/ViewModels/Dialogs/MessageBoxDialogViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace StripeCreator.WPF
+{
+    public class MessageBoxDialogViewModel : BaseViewModel
+    {
+        #region Public properties
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Сообщение для отображения
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Текст кнопки подтверждения
+        /// </summary>
+        public string OkText { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор с полной инициализацией
+        /// </summary>
+        /// <param name="title">Заголовок окна</param>
+        /// <param name="message">Сообщение для отображения</param>
+        /// <param name="okText">Текст кнопки подтверждения</param>
+        public MessageBoxDialogViewModel(string title, string message, string okText = "Да")
+        {
+            Title = title;
+            Message = message;
+            OkText = okText;
+        }
+
+        #endregion
+    }
+}
