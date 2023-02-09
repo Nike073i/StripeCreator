ad5105df995b100ebb36082dd148720a1aef2539
Nike073i <nike073i@mail.ru>
2023-02-09 19:28:46 +0400

    UiManager и реализация

---
+++ UI/StripeCreator.WPF/Services/Ui/DialogUiManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace StripeCreator.WPF
+{
+    /// <summary>
+    /// Диалоговая реаализация взаимодействия с пользователем
+    /// </summary>
+    public class DialogUiManager : IUiManager
+    {
+        #region Interface implementations
+
+        public Task ShowInfo(MessageBoxDialogViewModel viewModel)
+        {
+            return Task.FromResult(MessageBox.Show(viewModel.Message, viewModel.Title, MessageBoxButton.OK, MessageBoxImage.Information));
+        }
+
+        public Task ShowError(MessageBoxDialogViewModel viewModel)
+        {
+            return Task.FromResult(MessageBox.Show(viewModel.Message, viewModel.Title, MessageBoxButton.OK, MessageBoxImage.Error));
+        }
+
+        public Task<bool> ShowConfirm(MessageBoxDialogViewModel viewModel)
+        {
+            return Task.FromResult(MessageBox.Show(viewModel.Message, viewModel.Title, MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes);
+        }
+
+        #endregion
+    }
+}
---
+++ UI/StripeCreator.WPF/Services/Ui/IUiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace StripeCreator.WPF
+{
+    /// <summary>
+    /// Менеджер для интерактивного взаимодействия с пользователем
+    /// </summary>
+    public interface IUiManager
+    {
+        /// <summary>
+        /// Отобразить информацию для пользователя
+        /// </summary>
+        /// <param name="viewModel">ViewModel сообщения</param>
+        Task ShowInfo(MessageBoxDialogViewModel viewModel);
+
+        /// <summary>
+        /// Отобразить ошибку для пользователя
+        /// </summary>
+        /// <param name="viewModel">ViewModel сообщения</param>
+        Task ShowError(MessageBoxDialogViewModel viewModel);
+
+        /// <summary>
+        /// Отобразить окно подтверждения для пользователя
+        /// </summary>
+        /// <param name="viewModel">ViewModel сообщения</param>
+        /// <returns>Выбор пользователя</returns>
+        Task<bool> ShowConfirm(MessageBoxDialogViewModel viewModel);
+    }
+}
