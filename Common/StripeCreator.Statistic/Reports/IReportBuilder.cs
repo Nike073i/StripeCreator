baf2188ea4f354240fa01a4b9a9820ffb428e6e5
Nike073i <nike073i@mail.ru>
2023-02-19 20:06:56 +0400

    Добавление Migradoc, Создание интерфейса для отчетов

---
+++ Common/StripeCreator.Statistic/Reports/IReportBuilder.cs
@@ -0,0 +1,34 @@
+﻿using StripeCreator.Statistic.Models;
+
+namespace StripeCreator.Statistic.Reports
+{
+    /// <summary>
+    /// Интерфейс создателя отчетов
+    /// </summary>
+    public interface IReportBuilder
+    {
+        /// <summary>
+        /// Создание документа отчета
+        /// </summary>
+        void CreateDocument();
+
+        /// <summary>
+        /// Вставка данных по выручке
+        /// </summary>
+        /// <param name="data">Данные по выручке</param>
+        void InsertData(IEnumerable<OrderIncomeDataModel> data);
+
+        /// <summary>
+        /// Вставка данных по заказам в статусах
+        /// </summary>
+        /// <param name="data">Данные по заказам в статусах</param>
+        void InsertData(IEnumerable<OrderStatusDataModel> data);
+
+        /// <summary>
+        /// Сохранение отчета по пути <paramref name="path"/>
+        /// </summary>
+        /// <param name="path">Абсолютный путь сохранения отчета</param>
+        /// <returns>Результат сохранения</returns>
+        bool SaveDocument(string path);
+    }
+}
