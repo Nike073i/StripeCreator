4e0b21c4c3cb3ef406a7a3e107883f68e034080a
Nike073i <nike073i@mail.ru>
2023-03-09 13:43:57 +0400

    Добавление проекта Tools и Фабрики контекста Бд для dotnet ef

---
+++ UI/StripeCreator.WPF/Data/AppDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using StripeCreator.DAL;
+
+namespace StripeCreator.WPF.Data
+{
+    internal class AppDbContextFactory : IDesignTimeDbContextFactory<StripeCreatorDb>
+    {
+        private const string SqlServerMigrationAssembly = "StripeCreator.DAL.SqlServer";
+        private const string ConnectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = StripeCreatorDb; Integrated Security = True; Multiple Active Result Sets = True;";
+
+        public StripeCreatorDb CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StripeCreatorDb>();
+            var options = optionsBuilder
+                .UseSqlServer(ConnectionString, x => x.MigrationsAssembly(SqlServerMigrationAssembly))
+                .Options;
+            return new StripeCreatorDb(options);
+        }
+    }
+}
