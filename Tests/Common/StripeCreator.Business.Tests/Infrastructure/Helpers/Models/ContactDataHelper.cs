ef0fd351ec3fe4853e3cf3b3fb32789c2d04ee23
Nike073i <nike073i@mail.ru>
2023-03-11 19:25:59 +0400

    Тесты для сущности клиента

---
+++ Tests/Common/StripeCreator.Business.Tests/Infrastructure/Helpers/Models/ContactDataHelper.cs
@@ -0,0 +1,12 @@
+﻿using StripeCreator.Business.Models;
+
+namespace StripeCreator.Business.Tests.Infrastructure.Helpers.Models
+{
+    internal static class ContactDataHelper
+    {
+        public const string TestContactNumber = "+79475912545";
+        public const string TestEmail = "kit013i@mipl.ry";
+        public static ContactData CreateContactData(string contactNumber = TestContactNumber, string email = TestEmail) =>
+            new(contactNumber, email);
+    }
+}
