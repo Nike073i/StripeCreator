51abfdf169d83b0715ff8a3de5e04761f9c64114
Nike073i <nike073i@mail.ru>
2023-03-11 17:51:20 +0400

    Тесты для класса с данными по размеру

---
+++ Tests/Common/StripeCreator.Core.Tests/Models/ValueObjects/SizeTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using StripeCreator.Core.Models;
+
+namespace StripeCreator.Core.Tests.Models.ValueObjects
+{
+    internal class SizeTests
+    {
+        [Test]
+        [TestCase(100, 100)]
+        [TestCase(1, 100)]
+        [TestCase(1, 1)]
+        [TestCase(100, 1)]
+        public void CreateSize_ByWidthAndHeight_Correct(int width, int height)
+        {
+            var newSize = new Size(width, height);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newSize.Width, Is.EqualTo(width));
+                Assert.That(newSize.Height, Is.EqualTo(height));
+            });
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(-1, -1)]
+        [TestCase(0, 100)]
+        [TestCase(-1, 100)]
+        [TestCase(100, 0)]
+        [TestCase(100, -1)]
+        public void CreateSize_ByWidthAndHeight_NonPositiveParams(int width, int height)
+        {
+            void constructSize() => new Size(width, height);
+            Assert.Throws<ArgumentOutOfRangeException>(constructSize);
+        }
+    }
+}
