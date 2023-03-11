30e5a298649561aa0e998cae4757001368318bbd
Nike073i <nike073i@mail.ru>
2023-03-11 19:44:43 +0400

    Тесты для сущности продукта

---
+++ Tests/Common/StripeCreator.Business.Tests/Infrastructure/Helpers/Models/ProductHelper.cs
@@ -0,0 +1,14 @@
+﻿using StripeCreator.Business.Models;
+
+namespace StripeCreator.Business.Tests.Infrastructure.Helpers.Models
+{
+    internal static class ProductHelper
+    {
+        public const string TestName = "Продукт";
+        public const decimal TestPrice = 150m;
+        public const string TestDescription = "Описание";
+        public const string TestGuidString = "e9e43c3f-cb55-4877-854f-b92263948506";
+        public static Product CreateProduct(string name = TestName, decimal price = TestPrice, string description = TestDescription, Guid? id = null) =>
+            new(name, price, description, id ?? new Guid(TestGuidString));
+    }
+}
