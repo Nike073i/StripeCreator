fbed294da70c7cb02940900bd749d674cf694308
Nike073i <nike073i@mail.ru>
2023-03-11 19:44:43 +0400

    Тесты для сущности продукта

---
+++ Tests/Common/StripeCreator.Business.Tests/Models/Entities/ProductTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using StripeCreator.Business.Models;
+using StripeCreator.Business.Tests.Infrastructure.Helpers.Models;
+
+namespace StripeCreator.Business.Tests.Models.Entities
+{
+    internal class ProductTests
+    {
+        [Test]
+        [TestCaseSource(nameof(Create_Product_Correct_Cases))]
+        public void Create_Product_Correct(string name, decimal price, string description, Guid? id = null)
+        {
+            var product = new Product(name, price, description, id);
+            Assert.Multiple(() =>
+            {
+                Assert.That(product.Name, Is.EqualTo(name));
+                Assert.That(product.Price, Is.EqualTo(price));
+                Assert.That(product.Description, Is.EqualTo(description));
+                Assert.That(product.Id, Is.EqualTo(id));
+            });
+        }
+
+        static readonly object?[] Create_Product_Correct_Cases =
+        {
+            new object?[] { "Продукт", 150m, "Описание", null },
+            new object?[] { "Продукт", 1m, "Описание", null },
+            new object?[] { "Продукт", 150m, "Описание", new Guid("e9e43c3f-cb55-4877-854f-b92263948506") },
+        };
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-1000)]
+        public void Create_Product_IncorrectPrice(decimal price)
+        {
+            void construct() => ProductHelper.CreateProduct(price: price);
+            Assert.Throws<ArgumentOutOfRangeException>(construct);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("         ")]
+        public void Create_Product_IncorrectName(string name)
+        {
+            void construct() => ProductHelper.CreateProduct(name: name);
+            Assert.Throws<ArgumentNullException>(construct);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("         ")]
+        public void Create_Product_IncorrectDescription(string description)
+        {
+            void construct() => ProductHelper.CreateProduct(description: description);
+            Assert.Throws<ArgumentNullException>(construct);
+        }
+    }
+}
