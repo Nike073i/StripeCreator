3151b49ef7a705e1b5c349610a5f496bbd82058b
Nike073i <nike073i@mail.ru>
2023-03-11 19:25:59 +0400

    Тесты для сущности клиента

---
+++ Tests/Common/StripeCreator.Business.Tests/Infrastructure/Helpers/Models/PersonDataHelper.cs
@@ -0,0 +1,12 @@
+﻿using StripeCreator.Business.Models;
+
+namespace StripeCreator.Business.Tests.Infrastructure.Helpers.Models
+{
+    internal static class PersonDataHelper
+    {
+        public const string TestFirstName = "Никита";
+        public const string TestSecondName = "Филиппов";
+        public static PersonData CreatePersonData(string firstName = TestFirstName, string secondName = TestSecondName) =>
+            new(firstName, secondName);
+    }
+}
---
+++ Tests/Common/StripeCreator.Business.Tests/Models/Entities/ClientTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using StripeCreator.Business.Models;
+using StripeCreator.Business.Tests.Infrastructure.Helpers.Models;
+
+namespace StripeCreator.Business.Tests.Models.Entities
+{
+    internal class ClientTests
+    {
+        [Test]
+        [TestCaseSource(nameof(Create_Client_Correct_Cases))]
+        public void Create_Client_Correct(PersonData personData, ContactData contactData, Guid? id = null)
+        {
+            var newClient = new Client(personData, contactData, id);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newClient.PersonData, Is.EqualTo(personData));
+                Assert.That(newClient.ContactData, Is.EqualTo(contactData));
+                Assert.That(newClient.Id, Is.EqualTo(id));
+            });
+        }
+
+        static readonly object?[] Create_Client_Correct_Cases =
+        {
+            new object?[] { PersonDataHelper.CreatePersonData(), ContactDataHelper.CreateContactData(), null },
+            new object[] { PersonDataHelper.CreatePersonData(), ContactDataHelper.CreateContactData(), new Guid("e9e43c3f-cb55-4877-854f-b92263948506") },
+        };
+    }
+}
