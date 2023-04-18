44d756b67aafbcd23cd205184c988141c3c02b08
Nike073i <nike073i@mail.ru>
2023-04-18 16:41:00 +0400

    Добавление библиотеки FlaUi, попытки открытия приложения

---
+++ Tests/Ui/StripeCreator.WPF.Tests/Base/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using FlaUI.Core;
+using FlaUI.UIA3;
+using NUnit.Framework;
+using System;
+
+namespace StripeCreator.WPF.Tests.Base
+{
+    internal class BaseTest
+    {
+        protected string ApplicationRelativePath = @"T:\Repositories\Net\StripeCreatorClone\UI\StripeCreator.WPF\bin\Debug\net7.0-windows\";
+        protected string ApplicationExec = "StripeCreator.WPF.exe";
+
+        [SetUp]
+        protected void Start()
+        {
+            var app = Application.Launch(ApplicationRelativePath + ApplicationExec);
+            using var automation = new UIA3Automation();
+            var window = app.GetMainWindow(automation);
+            Console.WriteLine(window.Title);
+        }
+
+        [TearDown]
+        protected void Stop()
+        {
+        }
+    }
+}
---
+++ Tests/Ui/StripeCreator.WPF.Tests/Pages/WelcomePageTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using StripeCreator.WPF.Tests.Base;
+
+namespace StripeCreator.WPF.Tests.Pages
+{
+    internal class WelcomePageTests : BaseTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
