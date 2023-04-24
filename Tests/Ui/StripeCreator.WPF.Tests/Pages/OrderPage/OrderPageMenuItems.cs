d6b8cff88c45a09798ccb9a0bb91f0ef7ed1646f
Nike073i <nike073i@mail.ru>
2023-04-24 22:58:37 +0400

    Тесты навигации страницы заказов

---
+++ Tests/Ui/StripeCreator.WPF.Tests/Pages/OrderPage/OrderPageMenuItems.cs
@@ -0,0 +1,12 @@
+﻿namespace StripeCreator.WPF.Tests.Pages.OrderPage
+{
+    internal enum OrderPageMenuItems
+    {
+        Create = 0,
+        GetDetail = 1,
+        Update = 2,
+        Cancel = 3,
+        Refresh = 4,
+        ToMainMenu = 5,
+    }
+}
---
+++ Tests/Ui/StripeCreator.WPF.Tests/Pages/OrderPage/OrderPageUiElementSelectors.cs
@@ -0,0 +1,7 @@
+﻿namespace StripeCreator.WPF.Tests.Pages.OrderStorePage
+{
+    internal class OrderPageUiElementSelectors
+    {
+        public static readonly string CreateOrderWindowTitle = "Создание заказа";
+    }
+}
